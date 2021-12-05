ad60f464d45c4490e802aeace09c4d5aac627144
NCatalani <nicocatalani@gmail.com>
2021-12-05 20:41:40 -0300

    Added build script to automatically generate release file

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+LIBNAME=libcvec.a
+OUTPUT_DIR=output
+LIB_DIR=out
+RELEASE=""
+VERSION=$(cat README.md | head -n 1 | egrep -o -e '[0-9]{1}.[0-9]*')
+
+[ -n "$1" ] && RELEASE="Y"
+
+/usr/bin/make clean     &&
+/usr/bin/make
+
+[[ "$RELEASE" != "Y" ]] && exit 0
+
+OUTPUT="${OUTPUT_DIR}/libcvec_v${VERSION}.tar.gz"
+
+mkdir output/ 2>/dev/null
+
+tar -czvf ${OUTPUT} ${LIB_DIR}
